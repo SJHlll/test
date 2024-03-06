2441c9cadc0f35454210f00b758ab7f383eebd0a
SJHlll <ssaadd1212@gmail.com>
2024-03-06 09:40:56 +0900

    test001.드래곤볼

--- src/test/n001_Dragonball/Main.java
+++ src/test/n001_Dragonball/Main.java
@@ -19,6 +19,15 @@ public class Main {
 
             // 턴 진행, 내 행동 선택하기
             System.out.println("행동할 숫자를 입력하세요. (1 ~ 4)\n1. 기모으기 (1 sp 획득) || 2. 방어 (공격 방어) || 3. 공격 (1 sp 소모) || 4. 필살기 (3 sp 소모, 방어 무시)");
+
+            // 현재 기력 보여줌
+            System.out.printf("%s의 기력 : %d || %s의 기력 : %d\n"
+                    , player.name, player.energy, computer.name, computer.energy);
+
+            makeLine();
+
+            // 플레이어가 미리 행동을 선택, 행동하지는 않음
+            System.out.print("> ");
             int playerChoice = sc.nextInt();
 
             // 컴퓨터가 미리 행동, 플레이어가 선으로 행동하면 ai에 오류가 생김
@@ -43,6 +52,8 @@ public class Main {
                     break;
             }
 
+            makeLine();
+
 
             // 둘 다 공격을 날릴 시 체력 복구
             if (player.hp == 0 && computer.hp == 0) {
@@ -62,27 +73,28 @@ public class Main {
 
             // 끝나는 조건 (필살기 vs 공격)
             if (player.hp == -1 && computer.hp == 0) {
-                makeLine(); System.out.printf("%s의 승리!", computer.name); break;
+                makeLine();
+                System.out.printf("%s의 승리!", computer.name);
+                break;
             }
             if (player.hp == 0 && computer.hp == -1) {
-                makeLine(); System.out.printf("%s의 승리!", player.name); break;
+                makeLine();
+                System.out.printf("%s의 승리!", player.name);
+                break;
             }
 
             // 끝나는 조건 (기본)
             if (player.hp <= 0) {
-                makeLine(); System.out.printf("%s의 승리!", computer.name); break;
+                makeLine();
+                System.out.printf("%s의 승리!", computer.name);
+                break;
             }
             if (computer.hp <= 0) {
-                makeLine(); System.out.printf("%s의 승리!", player.name); break;
+                makeLine();
+                System.out.printf("%s의 승리!", player.name);
+                break;
             }
 
-            makeLine();
-
-            // 현재 기력 보여줌
-            System.out.printf("%s의 기력 : %d || %s의 기력 : %d\n"
-                    , player.name, player.energy, computer.name, computer.energy);
-
-            makeLine();
 
 
 
